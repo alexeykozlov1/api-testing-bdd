33df3fba75f7234cbc67ec8a0eb5fb1a8c4773d2
alexeykozlov1 <Lolo@2017>
2018-12-19 15:01:30 -0500

    api-test 1

--- src/test/java/com/project/api/CucumberRunner.java
+++ src/test/java/com/project/api/CucumberRunner.java
@@ -2,7 +2,7 @@ package com.project.api;
 
 import cucumber.api.CucumberOptions;
 
-import cucumber.api.junit.Cucumber;
+import cucumber.api.junit.Cucumber ;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
